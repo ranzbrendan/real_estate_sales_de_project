003804a167b7f694a99f4f635fafd37f88d53756
Ranz Brendan Gabor <ranzbrendan0330@gmail.com>
2024-09-26 12:27:23 +0000

    added staging of source table

---
+++ dbt/models/staging/stg_source_table.sql
@@ -0,0 +1,36 @@
+{{
+    config(
+        materialized='view'
+    )
+}}
+
+with source as (
+
+    select * from {{ source('staging', 'source_table') }}
+
+),
+
+renamed as (
+
+    select
+        sale_id
+        serial_number,
+        list_year,
+        date_recorded,
+        town,
+        address,
+        assessed_value,
+        sale_amount,
+        sales_ratio,
+        property_type,
+        residential_type,
+        non_use_code,
+        assessor_remarks,
+        opm_remarks,
+        location,
+
+    from source
+
+)
+
+select * from renamed
