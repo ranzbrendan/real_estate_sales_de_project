2ff9be8e7919e157d0b335937206cc0fdcafd6ae
Ranz Brendan Gabor <ranzbrendan0330@gmail.com>
2024-10-02 15:12:55 +0000

    done with data marts (hopefully)

--- dbt/models/data_marts/dm_median_sales_amount_per_property_type.sql
+++ dbt/models/data_marts/dm_median_sales_amount_per_property_type.sql
@@ -9,13 +9,16 @@ SELECT
     s.list_year,
     AVG(s.sale_amount) AS avg_sale_amount,
     AVG(s.assessed_value) AS avg_assessed_value,
-    (AVG(s.sale_amount) / NULLIF(AVG(s.assessed_value), 0)) AS avg_sales_ratio
+    APPROX_QUANTILES(s.sale_amount, 100)[OFFSET(50)] AS median_sale_amount,
+    APPROX_QUANTILES(s.assessed_value, 100)[OFFSET(50)] AS median_assessed_value
 FROM
     {{ ref("fact_sales") }} AS s
 LEFT JOIN {{ ref("dim_properties") }} AS p
     ON p.property_id = s.property_id
 LEFT JOIN {{ ref("dim_dates_recorded") }} AS d 
     ON d.date_id = s.date_recorded_id
+WHERE
+    s.sale_amount IS NOT NULL
 GROUP BY
     1, 2
 ORDER BY
--- dbt/models/data_marts/dm_median_sales_amount_per_town.sql
+++ dbt/models/data_marts/dm_median_sales_amount_per_town.sql
@@ -20,28 +20,32 @@ WITH loc AS (
 sale AS (
     SELECT
         s.list_year,
+        COALESCE(p.property_type, "Unknown") AS property_type,
         p.town,
-        AVG(s.sale_amount) AS avg_sale_amount
+        AVG(s.sale_amount) AS avg_sale_amount,
+        APPROX_QUANTILES(s.sale_amount, 100)[OFFSET(50)] AS median_sale_amount
     FROM 
-        {{ ref("fact_sales")}} AS s
-        LEFT JOIN {{ ref("dim_properties") }} AS p 
+        {{ ref("fact_sales") }} AS s
+    LEFT JOIN {{ ref("dim_properties") }} AS p 
         ON p.property_id = s.property_id 
     WHERE
         sale_amount IS NOT NULL
     GROUP BY
-        1, 2
+        1, 2, 3
 )   
 
 SELECT
     s.list_year,
+    s.property_type,
     s.town,
     CONCAT(l.latitude, ", ", l.longitude) AS location1,
     ST_GEOGFROMTEXT(CONCAT("POINT (", l.longitude, " ", l.latitude, ")")) AS location2,
-    s.avg_sale_amount
+    s.avg_sale_amount,
+    s.median_sale_amount
 FROM
     sale AS s
     INNER JOIN loc AS l 
     ON l.town = s.town
 ORDER BY
-    1, 2
+    1, 2, 3
     
